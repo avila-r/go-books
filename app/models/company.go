cd18b371c1d67c627eedf403cb3d098ec7dd792b
avila-r <avila.dev@outlook.com>
2024-09-16 23:27:50 -0300

    feat: change uuid to int64 as pk

--- app/models/company.go
+++ app/models/company.go
@@ -1,11 +1,7 @@
 package models
 
-import (
-	"github.com/google/uuid"
-)
-
 type Company struct {
-	ID          uuid.UUID `gorm:"primaryKey;type:uuid;default:gen_random_uuid()"`
+	ID          int64 `gorm:"primaryKey;type:uuid;default:gen_random_uuid()"`
 	Name        string
 	Description string
 	Image       string
