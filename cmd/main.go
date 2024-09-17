5e7159f86756e0fadb97235cf995c7a614a0c509
avila-r <avila.dev@outlook.com>
2024-09-16 23:30:09 -0300

    activate routes

--- cmd/main.go
+++ cmd/main.go
@@ -9,7 +9,10 @@ import (
 	"github.com/avila-r/books/test"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/gofiber/swagger"
+
 	"github.com/joho/godotenv"
 )
 
@@ -17,22 +20,35 @@ import (
 // @version 1.0
 // @description This is a RESTful API built-in Golang
 func main() {
-	loadEnv()
+	environment()
 
 	app := fiber.New()
 
-	app.Get("/docs", swagger.HandlerDefault)
-	app.Get("/", test.ReturnDataCollection)
-	app.Get("/book", test.GetBook)
-	routes.ActivatePublicRouter(app)
+	middleware(app)
+	routing(app)
 
 	url := os.Getenv("SERVER_URL")
 
 	app.Listen(url)
 }
 
-func loadEnv() {
+func environment() {
 	if err := godotenv.Load(); err != nil {
 		log.Fatalf("error loading .env file: %v", err)
 	}
+}
+
+func routing(app *fiber.App) {
+	app.Get("/docs", swagger.HandlerDefault)
+	app.Get("/", test.ReturnDataCollection)
+	app.Get("/book", test.GetBook)
+	routes.ActivatePublicRouter(app)
+}
+
+func middleware(app *fiber.App) {
+	app.Use(
+		cors.New(),
+		logger.New(),
+		// middlewares.JsonWebToken(),
+	)
 }
