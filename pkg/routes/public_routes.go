b5f8ab502e399ec9c338a63ec9ac916a8630626b
avila-r <avila.dev@outlook.com>
2024-09-18 00:46:37 -0300

    feat: add users' routes

--- pkg/routes/public_routes.go
+++ pkg/routes/public_routes.go
@@ -13,14 +13,19 @@ type PublicRouter struct {
 func ActivatePublicRouter(app *fiber.App) {
 	router := &PublicRouter{app}
 
-	router.bookRoutes()
+	router.activate()
 }
 
-func (r *PublicRouter) bookRoutes() {
+func (r *PublicRouter) activate() {
 	book_handler := handlers.NewBookHandler()
+	user_handler := handlers.NewUserHandler()
 
 	books := r.app.Group("/api/v1/books")
+	users := r.app.Group("/api/v1/users")
 
 	books.Get("/", book_handler.ListBooks)
 	books.Post("/", book_handler.InsertBook)
+
+	users.Get("/", user_handler.ListUsers)
+	users.Post("/", user_handler.RegisterUser)
 }
