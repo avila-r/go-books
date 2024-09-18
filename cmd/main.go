b5e21f4b6e6f2d5244d46adb19408027da3cc359
avila-r <avila.dev@outlook.com>
2024-09-18 19:26:21 -0300

    feat: improve error handling

--- cmd/main.go
+++ cmd/main.go
@@ -1,17 +1,14 @@
 package main
 
 import (
+	"errors"
 	"log"
 	"os"
 
 	_ "github.com/avila-r/books/docs"
 	"github.com/avila-r/books/pkg/routes"
-	"github.com/avila-r/books/test"
 
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/cors"
-	"github.com/gofiber/fiber/v2/middleware/logger"
-	"github.com/gofiber/swagger"
 
 	"github.com/joho/godotenv"
 )
@@ -20,35 +17,36 @@ import (
 // @version 1.0
 // @description This is a RESTful API built-in Golang
 func main() {
-	environment()
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("error loading .env file: %v", err)
+	}
 
-	app := fiber.New()
+	err_handler := func(c *fiber.Ctx, err error) error {
+		c.Set(
+			fiber.HeaderContentType,
+			fiber.MIMETextPlainCharsetUTF8,
+		)
 
-	middleware(app)
-	routing(app)
+		// Tries to parse 'error' as 'APIError'
+		var e *fiber.Error
+
+		if !errors.As(err, &e) {
+			// Status: Internal Server Error [500]
+			return c.Status(500).JSON(fiber.Map{
+				"error": err.Error(),
+			})
+		}
+
+		return c.Status(e.Code).JSON(e)
+	}
+
+	app := fiber.New(fiber.Config{
+		ErrorHandler: err_handler,
+	})
+
+	routes.Activate(app)
 
 	url := os.Getenv("SERVER_URL")
 
 	app.Listen(url)
-}
-
-func environment() {
-	if err := godotenv.Load(); err != nil {
-		log.Fatalf("error loading .env file: %v", err)
-	}
-}
-
-func routing(app *fiber.App) {
-	app.Get("/docs", swagger.HandlerDefault)
-	app.Get("/", test.ReturnDataCollection)
-	app.Get("/book", test.GetBook)
-	routes.ActivatePublicRouter(app)
-}
-
-func middleware(app *fiber.App) {
-	app.Use(
-		cors.New(),
-		logger.New(),
-		// middlewares.JsonWebToken(),
-	)
 }
