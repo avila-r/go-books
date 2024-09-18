6921533b5fe3c8c7e816852c866bda781c6006bc
avila-r <avila.dev@outlook.com>
2024-09-18 00:48:29 -0300

    fix: update role signature

--- app/models/user.go
+++ app/models/user.go
@@ -5,14 +5,20 @@ import (
 )
 
 type User struct {
-	ID    uuid.UUID `gorm:"primaryKey;type:uuid;default:gen_random_uuid()"`
-	Name  string
-	Email string
-	Roles []string `gorm:"type:json"`
+	ID       uuid.UUID `gorm:"primaryKey;type:uuid;default:gen_random_uuid()"`
+	Name     string
+	Email    string
+	Password string
+	Role     string
 }
 
 type UserResponse struct {
-	Name  string   `json:"name"`
-	Email string   `json:"email"`
-	Roles []string `json:"roles"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
+
+type UserRequest struct {
+	Name     string `json:"name" validate:"required"`
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required,min=8"`
 }
